eb07cf5a214aa741ac8be494d98dfc964ee4bbe8
leeraena <has_25116@hana.hs.kr>
2025-11-19 11:01:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("はじめのウェブアプリです!")
 name = st.text_input("なまえを入力してぐださい! : ")
-menu = st.selectbox("好きなことを選んで具ださい: ", ["アモンドボンボン"、"ままはアイリアン"])
+menu = st.selectbox("好きなことを選んで具ださい: ", ["アモンドボンボン", "ままはアイリアン"])
 if st.button("sentence make"):
   st.write(name + "さん、ごん日は。好きなテイストは" + menu + "ですね！")
   
